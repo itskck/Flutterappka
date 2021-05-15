3fe6c607d6152f560bfdc1d7d309ac5770caf4b4
Juliusz Piskor <juliuszpiskor@gmail.com>
2021-05-16 01:55:10 +0200

    Cud że działa.pptx

---
+++ android/app/src/main/kotlin/com/example/skladappka/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.skladappka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'dart:ui';
+
+/// Flutter code sample for BottomNavigationBar
+
+// This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
+// widget. The [BottomNavigationBar] has three [BottomNavigationBarItem]
+// widgets, which means it defaults to [BottomNavigationBarType.fixed], and
+// the [currentIndex] is set to index 0. The selected item is
+// amber. The `_onItemTapped` function changes the selected item's index
+// and displays a corresponding message in the center of the [Scaffold].
+//import 'dart:html';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,      
+      home: MyStatefulWidget(),
+      
+      
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 2;
+ 
+  
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30,fontFamily: 'Lobster-1.4');
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('yeeee',
+    style: optionStyle,
+    ),
+    Text(
+      'Index 1: Dodawanie',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Wczytywanie',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Porównywarka',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Logowanie',
+      style: optionStyle,
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(        
+        backgroundColor:Color.fromRGBO(102, 7, 8, 100),
+        //leading: Icon(Icons.computer),
+        title: Text('Składappka',
+        style: TextStyle(fontFamily: 'Lobster-1.4',fontWeight: FontWeight.w400,fontSize: 34),),       
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Dodaj zestaw',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.file_download),
+            label: 'Wczytaj zestaw',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Strona główna',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard),
+            label: 'Porównywarka'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Zaloguj')
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Color.fromRGBO(102, 7, 8,100) ,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
