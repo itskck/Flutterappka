e5d812c65977aa1284e10ce71900cc9b0b4b733f
Borokoro <marek0cichon@gmail.com>
2021-08-04 02:06:54 +0200

    Porzadek w kodzie

---
+++ lib/Porownywarka/Porownywarka.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:skladappka/main.dart';
+class Porownywarka extends StatefulWidget {
+  Porownywarka({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _Porownywarka createState() => _Porownywarka();
+}
+class _Porownywarka extends State<Porownywarka>{
+  int _selectedIndex=3;
+  void _onItemTapped(int index){
+    ktoro=index;
+    main();
+  }
+  @override
+  Widget build(BuildContext context) { //build context gives the layout, when you build widget it will always have this line
+    return Scaffold( //entry point to your app scaffold blank display
+      appBar: AppBar(
+        backgroundColor:Color.fromRGBO(102, 7, 8, 100),
+        //leading: Icon(Icons.computer),
+        title: Text('Składappka',
+          style: TextStyle(fontFamily: 'Lobster-1.4',fontWeight: FontWeight.w400,fontSize: 34),),
+      ),
+      body: Center(
+        child: Text("Porownywarka"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Dodaj zestaw',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.file_download),
+            label: 'Wczytaj zestaw',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Strona główna',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.leaderboard),
+              label: 'Porównywarka'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.login),
+              label: 'Zaloguj')
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Color.fromRGBO(102, 7, 8,100) ,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/dodawanieZestawu/dodaj.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:skladappka/main.dart';
+class dodaj extends StatefulWidget {
+  dodaj({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _dodaj createState() => _dodaj();
+}
+class _dodaj extends State<dodaj>{
+  int _selectedIndex=0;
+  void _onItemTapped(int index){
+    ktoro=index;
+    main();
+  }
+  @override
+  Widget build(BuildContext context) { //build context gives the layout, when you build widget it will always have this line
+    return Scaffold( //entry point to your app scaffold blank display
+      appBar: AppBar(
+        backgroundColor:Color.fromRGBO(102, 7, 8, 100),
+        //leading: Icon(Icons.computer),
+        title: Text('Składappka',
+          style: TextStyle(fontFamily: 'Lobster-1.4',fontWeight: FontWeight.w400,fontSize: 34),),
+      ),
+      body: Center(
+        child: Text("Dodawanko"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Dodaj zestaw',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.file_download),
+            label: 'Wczytaj zestaw',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Strona główna',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.leaderboard),
+              label: 'Porównywarka'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.login),
+              label: 'Zaloguj')
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Color.fromRGBO(102, 7, 8,100) ,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
