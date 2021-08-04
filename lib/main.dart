5fb70b1efb572b7b14c2c39a67675a3b4047bde7
Borokoro <marek0cichon@gmail.com>
2021-08-04 02:06:03 +0200

    Porzadek w kodzie

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,41 @@
-import 'dart:ui';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-/// Flutter code sample for BottomNavigationBar
-
-// This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
-// widget. The [BottomNavigationBar] has three [BottomNavigationBarItem]
-// widgets, which means it defaults to [BottomNavigationBarType.fixed], and
-// the [currentIndex] is set to index 0. The selected item is
-// amber. The `_onItemTapped` function changes the selected item's index
-// and displays a corresponding message in the center of the [Scaffold].
-//import 'dart:html';
 import 'package:flutter/material.dart';
-import 'homepage.dart';
-Future<void> main() async { 
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+import 'dodawanieZestawu/dodaj.dart';
+import 'Glowna/Glowna.dart';
+import 'Logowanie/Logowanie.dart';
+import 'Porownywarka/Porownywarka.dart';
+import 'wczytajZestaw/wczytajZestaw.dart';
+import 'package:flutter/material.dart';
+
+int ktoro=2;
+void main() {
+  return runApp(MaterialApp(
+    home: Skladapka(),
+  ));
 }
-Homepage homepage = new Homepage(); 
-String tekst = homepage.printdb();
-/// This is the main application widget.
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
-
+class Skladapka extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,      
-      home: MyStatefulWidget(),
-      
-      
+    if(ktoro==0) return MaterialApp(
+      title: 'Skladapka',
+      home: dodaj(title: 'dodawanie'),
     );
-  }
-}
-
-/// This is the stateful widget that the main application instantiates.
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 2;  
-  
-  
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30,fontFamily: 'Lobster-1.4');
-  static List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 1: Dodawanie',
-      style: optionStyle,
-    ),  
-    Text(
-      'Index 2: Wczytywanie',
-      style: optionStyle,
-    ),
-   Text(
-      "$tekst",
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Porównywarka',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 4:  Logowanie',
-      style: optionStyle,
-    )
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-   
-    return Scaffold(     
-      appBar: AppBar(        
-        backgroundColor:Color.fromRGBO(102, 7, 8, 100),
-        //leading: Icon(Icons.computer),
-        title: Text('Składappka',
-        style: TextStyle(fontFamily: 'Lobster-1.4',fontWeight: FontWeight.w400,fontSize: 34),),       
-      ),
-      body: Center(
-        
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'Dodaj zestaw',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.file_download),
-            label: 'Wczytaj zestaw',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Strona główna',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.leaderboard),
-            label: 'Porównywarka'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.login),
-            label: 'Zaloguj')
-        ],
-        currentIndex: _selectedIndex,
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Color.fromRGBO(102, 7, 8,100) ,
-        onTap: _onItemTapped,
-      ),
+    if(ktoro==1) return MaterialApp(
+      title: 'Skladapka',
+      home: wczytajZestaw(title: 'dodawanie'),
+    );
+    if(ktoro==2) return MaterialApp(
+      title: 'Skladapka',
+      home: Glowna(title: 'dodawanie'),
+    );
+    if(ktoro==3) return MaterialApp(
+      title: 'Skladapka',
+      home: Porownywarka(title: 'dodawanie'),
+    );
+    if(ktoro==4) return MaterialApp(
+      title: 'Skladapka',
+      home: Logowanie(title: 'dodawanie'),
     );
   }
 }
