33511c2f7a7a32cc0f5a8fff9a7492d1c6c1d11f
Juliusz Piskor <juliuszpiskor@gmail.com>
2021-08-17 01:04:16 +0200

    ao

--- lib/Glowna/Glowna.dart
+++ lib/Glowna/Glowna.dart
@@ -10,27 +10,9 @@ class Glowna extends StatefulWidget {
   @override
   _Glowna createState() => _Glowna();
 }
-class _Glowna extends State<Glowna>{
-  int _selectedIndex=2;
-  void _onItemTapped(int index){
-    globalna.ktoro=index;
-    inicjalizuj();
-  }
+class _Glowna extends State<Glowna>{ 
   @override
-  Widget build(BuildContext context) { //build context gives the layout, when you build widget it will always have this line
-    Icon moonIcon;
-    if (globalna.currentTheme == 0)
-      moonIcon = Icon(
-        Icons.brightness_2_outlined,
-        color: Colors.white,
-      );
-    else
-      moonIcon = Icon(
-        Icons.brightness_2,
-        color: Colors.white,
-      );
-      
-    //build context gives the layout, when you build widget it will always have this line
+  Widget build(BuildContext context) {
     return  Center(
         child: Text("Glowna"),
       );
