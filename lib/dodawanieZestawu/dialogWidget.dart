af7a2d5bdad215891cb5b4d9b0db9ca7dee5dc22
Juliusz Piskor <juliuszpiskor@gmail.com>
2021-08-30 20:18:19 +0200

    XD

--- lib/dodawanieZestawu/dialogWidget.dart
+++ lib/dodawanieZestawu/dialogWidget.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
-
+import 'package:skladappka/Firebase/Cpu.dart';
+import 'package:provider/provider.dart';
+import 'package:skladappka/Firebase/FireBase.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:core';
 class dialogWidget{
   
-    
-  void showPopup(BuildContext context,String component) => showDialog(
-    context: context,
-    builder: (BuildContext context) => popupWindow(context, component) 
-  );
+   Future<int> getListLength(){
+    Stream<List<Cpu>> list = FireBase().cpus;
+    return list.length;
+  }
   
-  Widget popupWindow(BuildContext context, String component) {
-    print('tiroro $component');
-    return SimpleDialog(      
+  Future<void> showPopup(BuildContext context,String component) async { 
+    int length = await getListLength();
+    showDialog(    
+    context: context,
+    builder: (BuildContext context) => popupWindow(context, component,length) 
+  );}
+  
+  Widget popupWindow(BuildContext context, String component, int length) {
+    
+    return StreamProvider<List<Cpu>>.value(
+      value: FireBase().cpus,
+      initialData: [],
+      child:
+    SimpleDialog(      
       title: Text('Choose your $component: '),
       children: [
+        for(int i=0;i< length ; i++)
         SimpleDialogOption(
           padding: EdgeInsets.symmetric(horizontal: 25,vertical: 25),
-          child: Text('some kind of $component'),
+          child: Text('gruszka'),
           onPressed: (){
             Navigator.pop(context);
-
+            
           },
         )
       ],      
       
-    );
+    ));
   }
 }
