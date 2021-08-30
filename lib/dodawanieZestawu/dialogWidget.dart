006e2ad580e8ee14793c6d2ec784372b3837b1fd
Borokoro <marek0cichon@gmail.com>
2021-08-30 21:47:08 +0200

    XDDDD

--- lib/dodawanieZestawu/dialogWidget.dart
+++ lib/dodawanieZestawu/dialogWidget.dart
@@ -6,7 +6,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dialogBuilder.dart';
 import 'dart:core';
 class dialogWidget{
-  final pomysl=pomyslXD();
    Future<int> getListLength(){
      print('hello');
     Stream<List<Cpu>> list = FireBase().cpus;
