32f5186a726518b4e6d0fae9b2b676aecd660b86
Juliusz Piskor <juliuszpiskor@gmail.com>
2021-09-03 23:32:27 +0200

    X kurde D

--- lib/dodawanieZestawu/dialogWidget.dart
+++ lib/dodawanieZestawu/dialogWidget.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:skladappka/Firebase/Case.dart';
+import 'package:skladappka/Firebase/Cooler.dart';
 import 'package:skladappka/Firebase/Cpu.dart';
 import 'package:provider/provider.dart';
+import 'package:skladappka/Firebase/Drive.dart';
 import 'package:skladappka/Firebase/FireBase.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:skladappka/Firebase/Gpu.dart';
+import 'package:skladappka/Firebase/Motherboard.dart';
+import 'package:skladappka/Firebase/Psu.dart';
+import 'package:skladappka/Firebase/Ram.dart';
 import 'dart:core';
 import 'dodaj.dart';
 import 'dialogBuilder.dart';
-class dialogWidget{
 
+class dialogWidget {
   /* Widget builder(String component,BuildContext context){
     final cpus = Provider.of<List<Cpu>>(context)??[];
     return SimpleDialog(
@@ -27,19 +34,67 @@ class dialogWidget{
 
     );
   } */
-  Future<void> showPopup(BuildContext context,String component) async {
+  Future<void> showPopup(BuildContext context, String component) async {
     await showDialog(
-    context: context,
-    builder: (BuildContext c) => popupWindow(c, component)
-  );}
+        context: context,
+        builder: (BuildContext c) => popupWindow(c, component));
+  }
 
-  
   Widget popupWindow(BuildContext context, String component) {
+    if (component == 'CPU')
+      return StreamProvider<List<Cpu>>.value(
+        value: FireBase().cpus,
+        initialData: [],
+        child: dialogBuilder(component: component),
+      );
 
-    return StreamProvider<List<Cpu>>.value(
-      value: FireBase().cpus,
-      initialData: [],
-      child: dialogBuilder(component:component),
-    );
+    if (component == 'PSU')
+      return StreamProvider<List<Psu>>.value(
+        value: FireBase().psus,
+        initialData: [],
+        child: dialogBuilder(component: component),
+      );
+
+    if (component == 'GPU')
+      return StreamProvider<List<Gpu>>.value(
+        value: FireBase().gpus,
+        initialData: [],
+        child: dialogBuilder(component: component),
+      );
+
+    if (component == 'CSTM COOLER')
+      return StreamProvider<List<Cooler>>.value(
+        value: FireBase().coolers,
+        initialData: [],
+        child: dialogBuilder(component: component),
+      );
+
+    if (component == 'MTBRD')
+      return StreamProvider<List<Motherboard>>.value(
+        value: FireBase().motherboards,
+        initialData: [],
+        child: dialogBuilder(component: component),
+      );
+
+    if (component == 'DRIVE')
+      return StreamProvider<List<Drive>>.value(
+        value: FireBase().drives,
+        initialData: [],
+        child: dialogBuilder(component: component),
+      );
+
+    if (component == 'CASE')
+      return StreamProvider<List<Case>>.value(
+        value: FireBase().cases,
+        initialData: [],
+        child: dialogBuilder(component: component),
+      );
+
+    if (component == 'RAM')
+      return StreamProvider<List<Ram>>.value(
+        value: FireBase().rams,
+        initialData: [],
+        child: dialogBuilder(component: component),
+      );
   }
 }
