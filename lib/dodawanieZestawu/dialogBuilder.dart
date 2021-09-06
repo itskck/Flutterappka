1aadaf7ec46e5da5fce291de538045b4e0770601
Juliusz Piskor <juliuszpiskor@gmail.com>
2021-09-07 00:49:09 +0200

    syf ogarnięty troche

--- lib/dodawanieZestawu/dialogBuilder.dart
+++ lib/dodawanieZestawu/dialogBuilder.dart
@@ -4,45 +4,24 @@ import 'package:skladappka/Firebase/Cooler.dart';
 import 'package:skladappka/Firebase/Cpu.dart';
 import 'package:provider/provider.dart';
 import 'package:skladappka/Firebase/Drive.dart';
-import 'package:skladappka/Firebase/FireBase.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:skladappka/Firebase/Gpu.dart';
 import 'package:skladappka/Firebase/Motherboard.dart';
 import 'package:skladappka/Firebase/Psu.dart';
 import 'package:skladappka/Firebase/Ram.dart';
 import 'dart:core';
-import 'package:skladappka/dodawanieZestawu/dialogWidget.dart';
 import 'dodaj.dart';
 class dialogBuilder extends StatelessWidget{
   String component;
-  var cpus,psus,gpus,coolers,mtbs,drives,cases,rams;
-  final d = new dodaj();
+  var cpus,psus,gpus,coolers,mtbs,drives,cases,rams;  
   dialogBuilder({this.component});
   @override
   Widget build(BuildContext context) {
     
-    if(component=='CPU')
+    if(component=='CPU'){
     cpus = Provider.of<List<Cpu>>(context)??[];
-    if(component=='PSU')
-    psus = Provider.of<List<Psu>>(context)??[];
-    if(component=='GPU')
-    gpus = Provider.of<List<Gpu>>(context)??[];
-    if(component=='CSTM COOLER')
-    coolers = Provider.of<List<Cooler>>(context)??[];
-    if(component=='MTBRD')
-    mtbs = Provider.of<List<Motherboard>>(context)??[];
-    if(component=='DRIVE')
-    drives = Provider.of<List<Drive>>(context)??[];
-    if(component=='CASE')
-    cases = Provider.of<List<Case>>(context)??[];
-    if(component=='RAM')
-    rams = Provider.of<List<Ram>>(context)??[];
-    
-    
     return SimpleDialog(
       title: Text('Choose your $component'),
-      children: [ 
-        if(component=='CPU')       
+      children: [             
         for(int i=0;i<cpus.length ; i++)
           SimpleDialogOption(
             padding: EdgeInsets.symmetric(horizontal: 25,vertical: 25),
@@ -51,9 +30,10 @@ class dialogBuilder extends StatelessWidget{
               dodaj.chosenCpu = cpus[i];
               Navigator.pop(context);
             },
-          ), 
-        if(component=='PSU')       
-        for(int i=0;i<psus.length ; i++)
+          )]);}
+    else if(component=='PSU'){
+    psus = Provider.of<List<Psu>>(context)??[];
+    for(int i=0;i<psus.length ; i++)
           SimpleDialogOption(
             padding: EdgeInsets.symmetric(horizontal: 25,vertical: 25),
             child: Text(psus[i].model.toString()),
@@ -61,8 +41,13 @@ class dialogBuilder extends StatelessWidget{
               dodaj.chosenPsu = psus[i];
               Navigator.pop(context);
             },
-          ),
-          if(component=='GPU')       
+          );}
+
+    if(component=='GPU'){
+    gpus = Provider.of<List<Gpu>>(context)??[];
+    return SimpleDialog(
+      title: Text('Choose your $component'),
+      children: [             
         for(int i=0;i<gpus.length ; i++)
           SimpleDialogOption(
             padding: EdgeInsets.symmetric(horizontal: 25,vertical: 25),
@@ -71,8 +56,13 @@ class dialogBuilder extends StatelessWidget{
               dodaj.chosenGpu = gpus[i];
               Navigator.pop(context);
             },
-          ),
-          if(component=='CSTM COOLER')       
+          )]);}
+
+    if(component=='CSTM COOLER'){
+    coolers = Provider.of<List<Cooler>>(context)??[];
+    return SimpleDialog(
+      title: Text('Choose your $component'),
+      children: [             
         for(int i=0;i<coolers.length ; i++)
           SimpleDialogOption(
             padding: EdgeInsets.symmetric(horizontal: 25,vertical: 25),
@@ -81,8 +71,13 @@ class dialogBuilder extends StatelessWidget{
               dodaj.chosenCooler = coolers[i];
               Navigator.pop(context);
             },
-          ),
-          if(component=='MTBRD')       
+          )]);}
+
+    if(component=='MTBRD'){
+    mtbs = Provider.of<List<Motherboard>>(context)??[];
+    return SimpleDialog(
+      title: Text('Choose your $component'),
+      children: [             
         for(int i=0;i<mtbs.length ; i++)
           SimpleDialogOption(
             padding: EdgeInsets.symmetric(horizontal: 25,vertical: 25),
@@ -91,8 +86,13 @@ class dialogBuilder extends StatelessWidget{
               dodaj.chosenMtb = mtbs[i];
               Navigator.pop(context);
             },
-          ),
-          if(component=='DRIVE')       
+          )]);}
+
+    if(component=='DRIVE'){
+    drives = Provider.of<List<Drive>>(context)??[];
+    return SimpleDialog(
+      title: Text('Choose your $component'),
+      children: [             
         for(int i=0;i<drives.length ; i++)
           SimpleDialogOption(
             padding: EdgeInsets.symmetric(horizontal: 25,vertical: 25),
@@ -101,8 +101,13 @@ class dialogBuilder extends StatelessWidget{
               dodaj.chosenDrive = drives[i];
               Navigator.pop(context);
             },
-          ),
-          if(component=='CASE')       
+          )]);}
+
+    if(component=='CASE'){
+    cases = Provider.of<List<Case>>(context)??[];
+    return SimpleDialog(
+      title: Text('Choose your $component'),
+      children: [             
         for(int i=0;i<cases.length ; i++)
           SimpleDialogOption(
             padding: EdgeInsets.symmetric(horizontal: 25,vertical: 25),
@@ -111,8 +116,13 @@ class dialogBuilder extends StatelessWidget{
               dodaj.chosenCase = cases[i];
               Navigator.pop(context);
             },
-          ),
-          if(component=='RAM')       
+          )]);}
+
+    if(component=='RAM'){
+    rams = Provider.of<List<Ram>>(context)??[];
+    return SimpleDialog(
+      title: Text('Choose your $component'),
+      children: [             
         for(int i=0;i<rams.length ; i++)
           SimpleDialogOption(
             padding: EdgeInsets.symmetric(horizontal: 25,vertical: 25),
@@ -121,9 +131,5 @@ class dialogBuilder extends StatelessWidget{
               dodaj.chosenRam = rams[i];
               Navigator.pop(context);
             },
-          )
-      ],
-
-    );
-  }
-}
+          )]);} 
+  }}
