cb8bf263054ada6c5a3960fec3f5584e2f1d03da
Juliusz Piskor <juliuszpiskor@gmail.com>
2021-09-13 02:06:18 +0200

    :trollface:

--- lib/Glowna/Glowna.dart
+++ lib/Glowna/Glowna.dart
@@ -1,9 +1,9 @@
+import 'dart:async';
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
-import 'package:skladappka/main.dart';
-import 'package:skladappka/Globalne.dart' as globalna;
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:skladappka/Firebase/Gpu.dart';
-import 'package:skladappka/Firebase/addToDatabase/addToDatabase.dart';
+
+
 class Glowna extends StatefulWidget {
   Glowna({Key key, this.title}) : super(key: key);
 
@@ -12,17 +12,56 @@ class Glowna extends StatefulWidget {
   @override
   _Glowna createState() => _Glowna();
 }
-class _Glowna extends State<Glowna>{
-  User _firebaseUser=FirebaseAuth.instance.currentUser;
+
+class _Glowna extends State<Glowna> {
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription<ConnectivityResult> connectivitySubscription;
+  ConnectivityResult connectivityResult;
+
+  @override
+  void initState() {
+    super.initState();
+    print('ao');
+    connectivitySubscription = _connectivity.onConnectivityChanged.listen(updateConnectionStatus);
+        print('object');
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+    connectivitySubscription.cancel();
+  }
+
+  void updateConnectionStatus(ConnectivityResult result){
+    setState(() {
+      print(result);
+      connectivityResult = result;
+    });
+  }
+
+  User _firebaseUser = FirebaseAuth.instance.currentUser;
+
+  Widget internetIcon() {
+    if (connectivityResult == ConnectivityResult.none)
+      return Icon(
+        Icons.wifi,
+        color: Colors.red,
+      );
+    else
+      return Icon(Icons.wifi, color: Colors.green);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Center(
+    return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          
+          Text("Witaj w składappce!"),
+          internetIcon(),
+          Text(connectivityResult.toString()),
         ],
-
       ),
-      );
+    );
   }
 }
