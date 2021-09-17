7756b53619a88d499b53738beb78417a087966d7
Borokoro <marek0cichon@gmail.com>
2021-09-17 20:52:23 +0200

    DAlsza praca z Porownywarka

--- lib/Porownywarka/dialogBuilderForCompare.dart
+++ lib/Porownywarka/dialogBuilderForCompare.dart
@@ -30,11 +30,25 @@ class dialogBuilderForCompare extends StatelessWidget{
               padding: EdgeInsets.symmetric(horizontal: 25,vertical: 25),
               child: Text(builds[i].timestamp),
               onPressed: () async{
-                if(ktoro==9) {
+                if(ktoro==0) {
                   Porownywarka.chosenCpu = await getFromSaved(id: builds[i].cpuId).getCpu();
+                  Porownywarka.chosenPsu = await getFromSaved(id: builds[i].cpuId).getPsu();
+                  Porownywarka.chosenMtb = await getFromSaved(id: builds[i].cpuId).getMotherboard();
+                  Porownywarka.chosenDrive = await getFromSaved(id: builds[i].cpuId).getDrive();
+                  Porownywarka.chosenRam = await getFromSaved(id: builds[i].cpuId).getRam();
+                  Porownywarka.chosenCase = await getFromSaved(id: builds[i].cpuId).getCase();
+                  Porownywarka.chosenGpu = await getFromSaved(id: builds[i].cpuId).getGpu();
+                  Porownywarka.chosenCooler = await getFromSaved(id: builds[i].cpuId).getCooler();
                 }
                 else{
-
+                  Porownywarka.chosenCpu2 = await getFromSaved(id: builds[i].cpuId).getCpu();
+                  Porownywarka.chosenPsu2 = await getFromSaved(id: builds[i].cpuId).getPsu();
+                  Porownywarka.chosenMtb2 = await getFromSaved(id: builds[i].cpuId).getMotherboard();
+                  Porownywarka.chosenDrive2 = await getFromSaved(id: builds[i].cpuId).getDrive();
+                  Porownywarka.chosenRam2 = await getFromSaved(id: builds[i].cpuId).getRam();
+                  Porownywarka.chosenCase2 = await getFromSaved(id: builds[i].cpuId).getCase();
+                  Porownywarka.chosenGpu2 = await getFromSaved(id: builds[i].cpuId).getGpu();
+                  Porownywarka.chosenCooler2 = await getFromSaved(id: builds[i].cpuId).getCooler();
                 }
                 Navigator.pop(context);
               },
