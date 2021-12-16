6f7b4771ef14c6299a6f0c4e9ed77eff6c9a719a
user <user@Mac-mini-user.local>
2021-12-16 16:14:04 +0100

    mac

--- lib/config/fileOperations.dart
+++ lib/config/fileOperations.dart
@@ -2,20 +2,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 
-class fileReader{
-  void save(String data, String title) async{
-    Directory appDocDir = await getExternalStorageDirectory();
-    String filePath = appDocDir.path + "/" +title+".txt";
+class fileReader {
+  void save(String data, String title) async {
+    Directory appDocDir = Platform.isAndroid
+        ? await getExternalStorageDirectory() //FOR ANDROID
+        : await getApplicationSupportDirectory();
+    ;
+    String filePath = appDocDir.path + "/" + title + ".txt";
     File f = File(filePath);
-    if(await exists(title)==false){
+    if (await exists(title) == false) {
       await f.create();
     }
     f.writeAsString(data);
   }
 
-  Future<bool> exists(String title) async{
-    Directory appDocDir = await getExternalStorageDirectory();
-    if(await File(appDocDir.path + "/" +title+".txt").exists()==true) {
+  Future<bool> exists(String title) async {
+    Directory appDocDir = Platform.isAndroid
+        ? await getExternalStorageDirectory()
+        : await getApplicationSupportDirectory();
+    if (await File(appDocDir.path + "/" + title + ".txt").exists() == true) {
       print("true");
       return true;
     } else {
@@ -24,14 +29,17 @@ class fileReader{
     }
   }
 
-   Future<String> read(String title) async{
-    Directory appDocDir = await getExternalStorageDirectory();
-    String filePath = appDocDir.path + "/" +title+".txt";
+  Future<String> read(String title) async {
+    Directory appDocDir = Platform.isAndroid
+        ? await getExternalStorageDirectory() //FOR ANDROID
+        : await getApplicationSupportDirectory();
+    ;
+    String filePath = appDocDir.path + "/" + title + ".txt";
     File f = File(filePath);
-    if(await exists(title)==false){
+    if (await exists(title) == false) {
       await f.create();
     }
-    String text=await f.readAsString();
+    String text = await f.readAsString();
     return text;
   }
 }
